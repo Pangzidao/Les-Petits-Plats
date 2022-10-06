f2d4daa3c91b022e27974d6b070e35e86e61b896
Nicolas Le Jeune <nicolasyvonlejeune@gmail.com>
2022-10-06 12:05:40 +0200

    search working

--- main.js
+++ main.js
@@ -4,23 +4,24 @@ const searchedElementInput = document.getElementById("elementSearched")
 let matchingRecipes = []
 let matchingElements = []
 let recipesIsMatching = []
+
 let listOfIngredientTagsWithDouble = []
 let listOfApplianceTagsWithDouble = []
 let listOfUstensilTagsWithDouble = []
 let listOfIngredientTags = []
 let listOfApplianceTags = []
 let listOfUstensilTags = []
+
 let searchedElements = []
 
-// getting tags
+// getting tags OK
 recipes.forEach(recipe =>{
     recipe.ingredients.forEach(e => listOfIngredientTagsWithDouble.push(e.ingredient.toLowerCase()))
     recipe.ustensils.forEach(e => listOfUstensilTagsWithDouble.push(e.toLowerCase()))
     listOfApplianceTagsWithDouble.push(recipe.appliance.toLowerCase())
 })
 
-
-//tags without double
+//tags without double OK
 
 function removingDouble(listOfTags){
     return listOfTags.filter((tag, index) => listOfTags.indexOf(tag) === index);
@@ -30,21 +31,122 @@ listOfIngredientTags = removingDouble(listOfIngredientTagsWithDouble)
 listOfApplianceTags = removingDouble(listOfApplianceTagsWithDouble);
 listOfUstensilTags = removingDouble(listOfUstensilTagsWithDouble)
 
+const generalListOfTags = {
+    "ingredientTags" : listOfIngredientTags,
+    "applianceTags" : listOfApplianceTags,
+    "ustensilTags" : listOfUstensilTags
+}
+
+const allTheTags = listOfIngredientTags.concat(listOfApplianceTags, listOfUstensilTags)
+
+
 // search bar
+
+searchedElementInput.addEventListener("input", function(){
+    searchBar()
+}) 
+
+let searchedElementsBar = []
 
 function searchBar(){
       // transformer la recherche en un tableau contenant les éléments recherchés
-      searchedElements.length = 0
-      let searchString = searchedElementInput.value.toLowerCase()
-      let searchedElementsBar = searchString.split(" ");
-      searchedElementsBar.forEach(e => searchedElements.push(e))        
-      search()
-
+      searchedElements.length = 0      
+      let searchString = searchedElementInput.value.toLowerCase().trim()
+      if (searchString.length > 2){
+        searchedElementsBar = searchString.split(" ");
+        search()
+    }else{
+        searchedElementsBar =[];
+        search()
+    }  
 }
+
+
+// research by tags
+
+// selecting tags field
+const searchByTagDOM = document.getElementsByClassName("searchByTag");
+
+
+Array.from(searchByTagDOM).forEach(element => {
+
+    let html = ""
+    element.addEventListener("focus", function(){
+        html = ""
+        let tagsType = element.id
+        const currentListOfTags = generalListOfTags[tagsType]
+        const currentTagFieldDOM = document.getElementById(`${tagsType}-display`)
+
+        element.addEventListener("input", function(e){
+            html=""
+            let searchInput = e.target.value;
+            let trimCurrentListOfTags = []
+
+            if (searchInput.length > 0){
+                trimCurrentListOfTags = currentListOfTags.filter(tag => tag.includes(searchInput))
+            }else{
+                trimCurrentListOfTags.length = 0
+            };
+
+            trimCurrentListOfTags.forEach(tag =>  html += `<p class="tagListElements" id="${tag}">${tag}</p>`)
+            currentTagFieldDOM.innerHTML = html;
+
+            let tagListElementsDOM = document.getElementsByClassName("tagListElements");
+
+            Array.from(tagListElementsDOM).forEach(tag =>{
+                tag.addEventListener("click", function(e){
+                    tagsSelected.push(e.target.id)
+                    tagSelected(tagsSelected)
+  
+                })
+            })
+
+        })
+
+    })
+
+});
+
+
+
+const tagsSelected = []
+
+function tagsSelectedDisplay(tags){
+    const tagsSelectedDOM = document.getElementById("tags-selected")
+
+    let html=""
+
+    tags.forEach(function(tag, index) {
+        html += `<p>${tag}</p><i class="fa-solid fa-xmark" onclick="removeTag('${index}')"></i>`
+    })
+
+    tagsSelectedDOM.innerHTML = html
+}
+
+function removeTag(index){
+    
+    tagsSelected.splice(index, 1)
+    console.log(tagsSelected)
+    tagSelected(tagsSelected)
+}
+
+function tagSelected(){
+    console.log(tagsSelected)
+    search()
+    tagsSelectedDisplay(tagsSelected)
+}
+
+
+
+
 
 // search function
 function search(){
-    
+
+    searchedElements = searchedElementsBar.concat(tagsSelected)
+    console.log(searchedElements)
+
+
     // emptying matching recipes array
     matchingRecipes.length = 0;
 
@@ -79,99 +181,13 @@ function search(){
         if (recipesIsMatching.includes(false)){
             return
         }else{
-            matchingRecipes.push(recipe.name)
+            matchingRecipes.push(recipe)
         }
     })
-    console.log(searchedElements)
     console.log(matchingRecipes)
 }
 
 
-// research by tags
-
-// selecting tags field
-const searchByTagDOM = document.getElementsByClassName("searchByTag");
-const generalListOfTags = {
-    "ingredientTags" : listOfIngredientTags,
-    "applianceTags" : listOfApplianceTags,
-    "ustensilTags" : listOfUstensilTags
-}
-
-Array.from(searchByTagDOM).forEach(element => {
-
-    let html = ""
-    element.addEventListener("focus", function(){
-        html = ""
-        let tagsType = element.id
-        const currentListOfTags = generalListOfTags[tagsType]
-        const currentTagFieldDOM = document.getElementById(`${tagsType}-display`)
-
-        element.addEventListener("input", function(e){
-            html=""
-            let searchInput = e.target.value;
-            let trimCurrentListOfTags = []
-
-            if (searchInput.length > 0){
-                trimCurrentListOfTags = currentListOfTags.filter(tag => tag.includes(searchInput))
-            }else{
-                trimCurrentListOfTags.length = 0
-            };
-
-            trimCurrentListOfTags.forEach(tag =>  html += `<p class="tagListElements" id="${tag}">${tag}</p>`)
-            currentTagFieldDOM.innerHTML = html;
-
-            let tagListElementsDOM = document.getElementsByClassName("tagListElements");
-
-            Array.from(tagListElementsDOM).forEach(tag =>{
-                tag.addEventListener("click", function(e){
-                    console.log(e.target.id)
-                    tagsSelected.push(e.target.id)
-                    console.log(tagsSelected)
-                    //tagsSelectedDisplay()
-                    searchedElements.push(...tagsSelected)
-                    search()
-                })
-            })
-
-        })
-
-
-    })
-
-
-    /*element.addEventListener("focusout", function(){
-        html = ""
-        let tagsType = element.id
-        const lastTagFieldDOM = document.getElementById(`${tagsType}-display`)
-        lastTagFieldDOM.innerHTML = html
-    })*/
-});
-
-const tagsSelected = []
-/*
-function tagsSelectedDisplay(tag){
-    const tagsSelectedDOM = document.getElementById("tags-selected")
-    tagsDisplay.push(tag)
-
-    console.log(tagsDisplay)
-
-    let html=""
-
-    tagsDisplay.forEach(function(tag, index) {
-        html += `<p>${tag}</p><i class="fa-solid fa-xmark" onclick="removeTag('${index}')"></i>`
-    })
-
-    tagsSelectedDOM.innerHTML = html
-}
-
-function removeTag(index){
-    
-    tagsDisplay.splice(index, 1)
-}
-
-
-
-*/
 
 
 
