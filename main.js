5c6414a2ff68bd54536ad781cd5f8f20312c408a
Nicolas Le Jeune <nicolasyvonlejeune@gmail.com>
2022-10-06 17:54:40 +0200

    css

--- main.js
+++ main.js
@@ -125,12 +125,10 @@ function tagsSelectedDisplay(tags){
 function removeTag(index){
     
     tagsSelected.splice(index, 1)
-    console.log(tagsSelected)
     tagSelected(tagsSelected)
 }
 
 function tagSelected(){
-    console.log(tagsSelected)
     search()
     tagsSelectedDisplay(tagsSelected)
 }
@@ -152,11 +150,18 @@ function search(){
         let listOfIngredients = []
         let ingredientsString = ""
 
+        let listOfUstensils = []
+        let ustensilsString = []
+
         // making ingredients list
         recipe.ingredients.forEach(e => listOfIngredients.push(e.ingredient.toLowerCase()))
+        recipe.ustensils.forEach(e => listOfUstensils.push(e.toLowerCase()))
+
 
         // converting ingredient list into a string
         ingredientsString = listOfIngredients.toString()
+        ustensilsString = listOfUstensils.toString()
+
 
         // emptying the array of matching recipes ex: [false, true] => []
         recipesIsMatching.length = 0;
@@ -166,6 +171,8 @@ function search(){
             if (recipe.name.toLowerCase().includes(searchedElement)
             ||recipe.description.toLowerCase().includes(searchedElement)
             ||ingredientsString.includes(searchedElement)
+            ||ustensilsString.includes(searchedElement)
+
             ){
                 recipesIsMatching.push(true)
             }else{
@@ -181,8 +188,62 @@ function search(){
         }
     })
     console.log(matchingRecipes)
+    recipesDisplay(matchingRecipes)
 }
 
+
+function recipesDisplay(recipes){
+
+    const recipesSection = document.getElementById("recipes");
+    
+    let html = ""
+
+    recipes.forEach(recipe =>{
+        
+        let ingredientHtml = ""
+
+        recipe.ingredients.forEach(i => {
+            if("quantity" in i === false){
+                ingredientHtml +=`
+            <p>${i.ingredient}</p>
+            `
+            }else if("unit" in i === false){
+                ingredientHtml +=`
+            <p>${i.ingredient}: ${i.quantity}</p>
+            `                  
+            }else{
+                ingredientHtml += `
+                <p>${i.ingredient}: ${i.quantity} ${i.unit}</p>
+                `  
+            } 
+        })
+
+        html += `
+            <div class="recipe-card">
+                <div class="photo-holder"></div>
+                <div class="recipe-text">
+                    <div class="recipe-header">
+                        <h2>${recipe.name}</h2>
+                        <p class="recipe-time"><i class="fa-regular fa-clock"></i>${recipe.time} min</p>
+                    </div>
+
+                    <div class="recipe-main-text">
+                        <div class="ingredients-list">${ingredientHtml}</div>
+                        <p class="recipe-description">${recipe.description}</p>
+                    </div>
+
+                    
+                </div>
+
+            </div>
+        `
+    })
+
+    recipesSection.innerHTML = html
+
+}
+
+recipesDisplay(recipes)
 
 
 
