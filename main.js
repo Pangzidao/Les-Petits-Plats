8fdc287b6cfe9b5c43bb96de8ec4ef35682c5912
Nicolas Le Jeune <nicolasyvonlejeune@gmail.com>
2022-10-17 10:23:09 +0200

    recipesDisplay

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
 console.log(recipes)
+
+
+
+
+function recipesDisplay(recipes){
+
+    const recipesSection = document.getElementById("recipes");
+    
+    let html = ""
+
+    recipes.forEach(recipe =>{
+        
+        let ingredientHtml = ""
+
+        recipe.ingredients.forEach(i => {
+            if("quantity" in i === false){
+                ingredientHtml +=`
+            <p>${i.ingredient}</p>
+            `
+            }else if("unit" in i === false){
+                ingredientHtml +=`
+            <p>${i.ingredient}: ${i.quantity}</p>
+            `                  
+            }else{
+                ingredientHtml += `
+                <p>${i.ingredient}: ${i.quantity} ${i.unit}</p>
+                `  
+            } 
+        })
+
+        html += `
+            <div class="recipe-card">
+                <div class="photo-holder"></div>
+                <div class="recipe-text">
+                    <div class="recipe-header">
+                        <h2>${recipe.name}</h2>
+                        <p class="recipe-time"><i class="fa-regular fa-clock"></i>${recipe.time} min</p>
+                    </div>
+
+                    <div class="recipe-main-text">
+                        <div class="ingredients-list">${ingredientHtml}</div>
+                        <p class="recipe-description">${recipe.description}</p>
+                    </div>
+
+                    
+                </div>
+
+            </div>
+        `
+    })
+
+    recipesSection.innerHTML = html
+
+}
+
+recipesDisplay(recipes)
